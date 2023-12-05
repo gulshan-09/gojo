3cd2efb247cff6f2bc2caa5627828a6b856dc46e
gulshan-09 <152474356+gulshan-09@users.noreply.github.com>
2023-12-05 23:38:48 +0530

    Update embedMovieSchema.js

--- models/embedMovieSchema.js
+++ models/embedMovieSchema.js
@@ -4,55 +4,46 @@ const mongoose = require('mongoose');
 const embedMovieJson = new mongoose.Schema({
     id:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     embed_title:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     myCloud:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     fileMoon:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     vTube:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     superEmbed:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     embed:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     vidPlay1:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
     vidPlay2:{
         type: String,
-        unique: true,
         required: true,
         trim: true,
     },
